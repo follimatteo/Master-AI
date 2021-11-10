da1fad7c8e0602b5f7585bbbf3fc3f6a34cfaffa
Matteo Folli <folli_matteo@outlook.it>
2021-11-10 17:24:58 +0100

    ADD Parser for NQueens

--- ConstraintProgramming/NQueens.py
+++ ConstraintProgramming/NQueens.py
@@ -1,9 +1,20 @@
 from minizinc import Instance, Model, Solver
 import time
+import argparse
 
-NQ = 12
-allsol = True
-print_result = False
+parser = argparse.ArgumentParser(description='Nqueens Problem solver with MiniZinc solver')
+parser.add_argument('N', nargs='?', type=int, default=5)
+parser.add_argument('--models', nargs='*', default='all', help="choose between ['raw', 'alldiff', 'sym'] or default 'all' ")
+parser.add_argument('--all_solutions',  default=False, help='if FALSE stop at the first solution')
+parser.add_argument('--print_board',  default=False, help='if TRUE print all the results board')
+
+args = parser.parse_args()
+
+print(args)
+NQ = args.N
+allsol = args.all_solutions
+print_board = args.print_board
+MODELS = args.models
 
 def print_chessboard(chessboard):
     for row in chessboard:
@@ -16,91 +27,93 @@ def print_chessboard(chessboard):
 
 
 # NQueens basic resolution
-nqueens = Model()
-nqueens.add_string(
-    '''
-    int: n; % The number of queens.
+if 'raw' in MODELS or MODELS =='all':
+    nqueens = Model()
+    nqueens.add_string(
+        '''
+        int: n; % The number of queens.
 
-    array[1..n,1..n] of var bool: qb;
+        array[1..n,1..n] of var bool: qb;
 
-    array [1..n] of var 1..n: q;
+        array [1..n] of var 1..n: q;
 
-    include "alldifferent.mzn";
+        include "alldifferent.mzn";
 
-    constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
+        constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
 
-    constraint alldifferent(q);
-    constraint
-        forall(i in 1..n)(
-            forall(j in i+1..n)(
-                abs(q[i]-q[j])!=abs(i-j)
-            )
+        constraint alldifferent(q);
+        constraint
+            forall(i in 1..n)(
+                forall(j in i+1..n)(
+                    abs(q[i]-q[j])!=abs(i-j)
+                )
+        )
+        '''
     )
-    '''
-)
 
-# Find the MiniZinc solver configuration for Gecode
-gecode = Solver.lookup("gecode")
-# Create an Instance of the n-Queens model for Gecode
-instance = Instance(gecode, nqueens)
-# Assign 4 to n
-instance["n"] = NQ
-start_time = time.time()
-result = instance.solve(all_solutions=allsol)
-# Output the array q
-print("\n")
-print("Resolved in --- %s seconds ---" % (time.time() - start_time))
-if allsol:
-    print(f'Number of solution: {len(result)}')
-    if print_result:
-        for i in range(len(result)):
-            print(f'Solution: {i+1}')
-            print_chessboard(result[i, 'qb'])
-            print('\n')
-else:
-    print_chessboard(result["qb"])
+    # Find the MiniZinc solver configuration for Gecode
+    gecode = Solver.lookup("gecode")
+    # Create an Instance of the n-Queens model for Gecode
+    instance = Instance(gecode, nqueens)
+    # Assign 4 to n
+    instance["n"] = NQ
+    start_time = time.time()
+    result = instance.solve(all_solutions=allsol)
+    # Output the array q
+    print("\n")
+    print("Resolved in --- %s seconds ---" % (time.time() - start_time))
+    if allsol:
+        print(f'Number of solution: {len(result)}')
+        if print_board:
+            for i in range(len(result)):
+                print(f'Solution: {i+1}')
+                print_chessboard(result[i, 'qb'])
+                print('\n')
+    else:
+        print_chessboard(result["qb"])
 
 
 # NQueens all different model
-nqueens = Model()
-nqueens.add_string(
-    '''
-    int: n; % The number of queens.
+if 'alldiff' in MODELS or MODELS =='all':
+    nqueens = Model()
+    nqueens.add_string(
+        '''
+        int: n; % The number of queens.
 
-    array[1..n,1..n] of var bool: qb;
+        array[1..n,1..n] of var bool: qb;
 
-    array [1..n] of var 1..n: q;
+        array [1..n] of var 1..n: q;
 
-    include "alldifferent.mzn";
+        include "alldifferent.mzn";
 
-    constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
+        constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
 
-    constraint alldifferent(q);
-    constraint alldifferent(i in 1..n)(q[i] + i);
-    constraint alldifferent(i in 1..n)(q[i] - i);
-    '''
-)
+        constraint alldifferent(q);
+        constraint alldifferent(i in 1..n)(q[i] + i);
+        constraint alldifferent(i in 1..n)(q[i] - i);
+        '''
+    )
 
-# Find the MiniZinc solver configuration for Gecode
-gecode = Solver.lookup("gecode")
-# Create an Instance of the n-Queens model for Gecode
-instance = Instance(gecode, nqueens)
-# Assign 4 to n
-instance["n"] = NQ
-start_time = time.time()
-result = instance.solve(all_solutions=allsol)
-# Output the array q
-print("\n")
-print("Resolved in --- %s seconds ---" % (time.time() - start_time))
-if allsol:
-    print(f'Number of solution: {len(result)}')
-    if print_result:
-        for i in range(len(result)):
-            print(f'Solution: {i+1}')
-            print_chessboard(result[i, 'qb'])
-            print('\n')
-else:
-    print_chessboard(result["qb"])
+    # Find the MiniZinc solver configuration for Gecode
+    gecode = Solver.lookup("gecode")
+    # Create an Instance of the n-Queens model for Gecode
+    instance = Instance(gecode, nqueens)
+    # Assign 4 to n
+    instance["n"] = NQ
+    start_time = time.time()
+    result = instance.solve(all_solutions=allsol)
+    # Output the array q
+    print("\n")
+    print("Resolved in --- %s seconds ---" % (time.time() - start_time))
+    if allsol:
+        print(f'Number of solution: {len(result)}')
+        if print_board:
+            for i in range(len(result)):
+                print(f'Solution: {i+1}')
+                print_chessboard(result[i, 'qb'])
+                print('\n')
+    else:
+        print_chessboard(result["qb"])
 
 #
 #   Breaking simmetry
@@ -108,53 +121,54 @@ else:
 #   2' : fzn_lex_lesseq_bool([ qb[i,j] | i,j in 1..n ], [ qb[j,i] | i,j in 1..n ])  ----> THIS BREAK SIMMITRY WITH LEXOGRAPIC ORDER RESPECT TO DIAGONAL?
 #
 #
-nqueens = Model()
-nqueens.add_string(
-    '''
-    int: n; % The number of queens.
+if 'sym' in MODELS or MODELS =='all':
+    nqueens = Model()
+    nqueens.add_string(
+        '''
+        int: n; % The number of queens.
 
-    array[1..n,1..n] of var bool: qb;
+        array[1..n,1..n] of var bool: qb;
 
-    array [1..n] of var 1..n: q;
+        array [1..n] of var 1..n: q;
 
-    include "alldifferent.mzn";
-    include "fzn_lex_lesseq_bool.mzn";
+        include "alldifferent.mzn";
+        include "fzn_lex_lesseq_bool.mzn";
 
-    constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
+        constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
 
-    constraint alldifferent(q);
-    constraint alldifferent(i in 1..n)(q[i] + i);
-    constraint alldifferent(i in 1..n)(q[i] - i);
+        constraint alldifferent(q);
+        constraint alldifferent(i in 1..n)(q[i] + i);
+        constraint alldifferent(i in 1..n)(q[i] - i);
 
-    %constraint q[1] <= n div 2;
-    constraint
-        fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i,j in 1..n ])
-        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i in reverse(1..n), j in 1..n ])
-        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i in 1..n, j in reverse(1..n) ])
-        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i in 1..n, j in reverse(1..n) ])
-        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i in reverse(1..n), j in 1..n ])
-        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i,j in reverse(1..n) ])
-        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i,j in reverse(1..n) ]);
-    '''
-)
+        %constraint q[1] <= n div 2;
+        constraint
+            fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i,j in 1..n ])
+            /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i in reverse(1..n), j in 1..n ])
+            /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i in 1..n, j in reverse(1..n) ])
+            /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i in 1..n, j in reverse(1..n) ])
+            /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i in reverse(1..n), j in 1..n ])
+            /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i,j in reverse(1..n) ])
+            /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i,j in reverse(1..n) ]);
+        '''
+    )
 
-# Find the MiniZinc solver configuration for Gecode
-gecode = Solver.lookup("gecode")
-# Create an Instance of the n-Queens model for Gecode
-instance = Instance(gecode, nqueens)
-# Assign 4 to n
-instance["n"] = NQ
-start_time = time.time()
-result = instance.solve(all_solutions=allsol)
-# Output the array q
-print("\n")
-print("Resolved in --- %s seconds ---" % (time.time() - start_time))
-if allsol:
-    print(f'Number of solution: {len(result)}')
-    if print_result:
-        for i in range(len(result)):
-            print(f'Solution: {i+1}')
-            print_chessboard(result[i, 'qb'])
-            print('\n')
-else:
-    print_chessboard(result["qb"])
+    # Find the MiniZinc solver configuration for Gecode
+    gecode = Solver.lookup("gecode")
+    # Create an Instance of the n-Queens model for Gecode
+    instance = Instance(gecode, nqueens)
+    # Assign 4 to n
+    instance["n"] = NQ
+    start_time = time.time()
+    result = instance.solve(all_solutions=allsol)
+    # Output the array q
+    print("\n")
+    print("Resolved in --- %s seconds ---" % (time.time() - start_time))
+    if allsol:
+        print(f'Number of solution: {len(result)}')
+        if print_board:
+            for i in range(len(result)):
+                print(f'Solution: {i+1}')
+                print_chessboard(result[i, 'qb'])
+                print('\n')
+    else:
+        print_chessboard(result["qb"])
