7c3396493751e44914dba422e9c1e2fcf21257c3
Matteo Folli <folli_matteo@outlook.it>
2021-11-10 16:26:50 +0100

    Add NQ Results table

--- ConstraintProgramming/NQueens.py
+++ ConstraintProgramming/NQueens.py
@@ -1,6 +1,19 @@
 from minizinc import Instance, Model, Solver
 import time
 
+NQ = 12
+allsol = True
+print_result = False
+
+def print_chessboard(chessboard):
+    for row in chessboard:
+        for el in row:
+            if el:
+                print('Q', end=' ')
+            else:
+                print('_', end=' ')
+        print()
+
 
 # NQueens basic resolution
 nqueens = Model()
@@ -8,9 +21,13 @@ nqueens.add_string(
     '''
     int: n; % The number of queens.
 
+    array[1..n,1..n] of var bool: qb;
+
     array [1..n] of var 1..n: q;
 
     include "alldifferent.mzn";
+
+    constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
 
     constraint alldifferent(q);
     constraint
@@ -27,13 +44,21 @@ gecode = Solver.lookup("gecode")
 # Create an Instance of the n-Queens model for Gecode
 instance = Instance(gecode, nqueens)
 # Assign 4 to n
-instance["n"] = 4
+instance["n"] = NQ
 start_time = time.time()
-result = instance.solve()
+result = instance.solve(all_solutions=allsol)
 # Output the array q
 print("\n")
 print("Resolved in --- %s seconds ---" % (time.time() - start_time))
-print(result["q"])
+if allsol:
+    print(f'Number of solution: {len(result)}')
+    if print_result:
+        for i in range(len(result)):
+            print(f'Solution: {i+1}')
+            print_chessboard(result[i, 'qb'])
+            print('\n')
+else:
+    print_chessboard(result["qb"])
 
 
 # NQueens all different model
@@ -42,9 +67,13 @@ nqueens.add_string(
     '''
     int: n; % The number of queens.
 
+    array[1..n,1..n] of var bool: qb;
+
     array [1..n] of var 1..n: q;
 
     include "alldifferent.mzn";
+
+    constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
 
     constraint alldifferent(q);
     constraint alldifferent(i in 1..n)(q[i] + i);
@@ -57,10 +86,75 @@ gecode = Solver.lookup("gecode")
 # Create an Instance of the n-Queens model for Gecode
 instance = Instance(gecode, nqueens)
 # Assign 4 to n
-instance["n"] = 4
+instance["n"] = NQ
 start_time = time.time()
-result = instance.solve()
+result = instance.solve(all_solutions=allsol)
 # Output the array q
 print("\n")
 print("Resolved in --- %s seconds ---" % (time.time() - start_time))
-print(result["q"])
+if allsol:
+    print(f'Number of solution: {len(result)}')
+    if print_result:
+        for i in range(len(result)):
+            print(f'Solution: {i+1}')
+            print_chessboard(result[i, 'qb'])
+            print('\n')
+else:
+    print_chessboard(result["qb"])
+
+#
+#   Breaking simmetry
+#   1' : constraint q[1] <= n div 2  ------> THIS FORCE THE QUEEN IN THE FIRST COLUMN TO BE IN THE FIRST HALF, BREAKING VERTICAL SIMMETRY
+#   2' : fzn_lex_lesseq_bool([ qb[i,j] | i,j in 1..n ], [ qb[j,i] | i,j in 1..n ])  ----> THIS BREAK SIMMITRY WITH LEXOGRAPIC ORDER RESPECT TO DIAGONAL?
+#
+#
+nqueens = Model()
+nqueens.add_string(
+    '''
+    int: n; % The number of queens.
+
+    array[1..n,1..n] of var bool: qb;
+
+    array [1..n] of var 1..n: q;
+
+    include "alldifferent.mzn";
+    include "fzn_lex_lesseq_bool.mzn";
+
+    constraint forall (i,j in 1..n) ( qb[i,j] <-> (q[i]=j) );
+
+    constraint alldifferent(q);
+    constraint alldifferent(i in 1..n)(q[i] + i);
+    constraint alldifferent(i in 1..n)(q[i] - i);
+
+    %constraint q[1] <= n div 2;
+    constraint
+        fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i,j in 1..n ])
+        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i in reverse(1..n), j in 1..n ])
+        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i in 1..n, j in reverse(1..n) ])
+        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i in 1..n, j in reverse(1..n) ])
+        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i in reverse(1..n), j in 1..n ])
+        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[i,j] | i,j in reverse(1..n) ])
+        /\  fzn_lex_lesseq_bool(array1d(qb), [ qb[j,i] | i,j in reverse(1..n) ]);
+    '''
+)
+
+# Find the MiniZinc solver configuration for Gecode
+gecode = Solver.lookup("gecode")
+# Create an Instance of the n-Queens model for Gecode
+instance = Instance(gecode, nqueens)
+# Assign 4 to n
+instance["n"] = NQ
+start_time = time.time()
+result = instance.solve(all_solutions=allsol)
+# Output the array q
+print("\n")
+print("Resolved in --- %s seconds ---" % (time.time() - start_time))
+if allsol:
+    print(f'Number of solution: {len(result)}')
+    if print_result:
+        for i in range(len(result)):
+            print(f'Solution: {i+1}')
+            print_chessboard(result[i, 'qb'])
+            print('\n')
+else:
+    print_chessboard(result["qb"])
--- ConstraintProgramming/README.md
+++ ConstraintProgramming/README.md
@@ -1,3 +1,40 @@
 Constraint PRogramming EXAM Repository
 
 Done with MiniZinc framework for python3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+N Queens printed result:
+
+| Model:         | NQueens:       |  Time:         | N. Solutions:  |
+| :------------- | :------------- | :------------- | :------------- |
+| Raw Model      |  8             | 0.5680 s       | 92             |
+| alldiff Model  |  8             | 0.5360 s       | 92             |
+| sym Model      |  8             | 0.5649 s       | 12             |
+| :------------- | :------------- | :------------- | :------------- |
+| Raw Model      |  9             | 0.6439 s       | 352            |
+| alldiff Model  |  9             | 0.5909 s       | 352            |
+| sym Model      |  9             | 0.5460 s       | 46             |
+| :------------- | :------------- | :------------- | :------------- |
+| Raw Model      |  10            | 1.0390 s       | 724            |
+| alldiff Model  |  10            | 0.7270 s       | 724            |
+| sym Model      |  10            | 0.5870 s       | 92             |
+| :------------- | :------------- | :------------- | :------------- |
+| Raw Model      |  11            | 3.2809 s       | 2680           |
+| alldiff Model  |  11            | 1.4690 s       | 2680           |
+| sym Model      |  11            | 0.7090 s       | 341            |
+| :------------- | :------------- | :------------- | :------------- |
+| Raw Model      |  12            | 18.0070 s      | 14200          |
+| alldiff Model  |  12            | 6.3239 s       | 14200          |
+| sym Model      |  12            | 1.6179 s       | 1787           |
