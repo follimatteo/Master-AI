ed414c361aa18928a86fe23b8239d64dae296327
Matteo Folli <folli_matteo@outlook.it>
2022-12-20 16:28:25 +0100

    Update ARGS.py

--- ML4CV/ARGS.py
+++ ML4CV/ARGS.py
@@ -29,6 +29,6 @@ class ARGS():
         self.output_dir = ''
         self.saveckp_freq = ''
         self.seed = 12
-        self.num_workers = 5
+        self.num_workers = 2
         self.dist_url = 'env://'
         self.local_rank = 0
